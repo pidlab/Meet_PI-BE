e1ac5585866a6e051e187ce373343762cec81fca
go-tiger <inter_tiger_@naver.com>
2024-01-13 15:51:48 +0900

    init: 초기 세팅

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const development = {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+};
+
+const production = {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+};
+
+const test = {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+};
+
+module.exports = { development, production, test };
